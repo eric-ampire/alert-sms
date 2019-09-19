4b440bb55c8c109773767faa948db2010aa2290f
Eric Ampire <ericampire.top@gmail.com>
2019-09-19 07:53:44 +0200

    Connect App class to Manifest

---
+++ app/src/main/java/com/ericampire/mobile/alertsms/App.kt
@@ -0,0 +1,14 @@
+package com.ericampire.mobile.alertsms
+
+import android.app.Application
+import android.content.Context
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
+    }
+}
