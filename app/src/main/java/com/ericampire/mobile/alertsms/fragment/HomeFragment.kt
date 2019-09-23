2e2c83467e919848265f7ed21940ade49d1b3b97
Eric Ampire <ericampire.top@gmail.com>
2019-09-23 11:05:56 +0200

    Adding HomeFragment

---
+++ app/src/main/java/com/ericampire/mobile/alertsms/fragment/HomeFragment.kt
@@ -0,0 +1,21 @@
+package com.ericampire.mobile.alertsms.fragment
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.ericampire.mobile.alertsms.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, parent: ViewGroup?, state: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return null
+    }
+}
