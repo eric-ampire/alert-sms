b77a08b44bf11ceca9a6e1aee14cbd4221b9935d
Eric Ampire <ericampire.top@gmail.com>
2019-09-23 17:23:36 +0200

    Main Activity use databinding

--- app/src/main/java/com/ericampire/mobile/alertsms/MainActivity.kt
+++ app/src/main/java/com/ericampire/mobile/alertsms/MainActivity.kt
@@ -2,13 +2,9 @@ package com.ericampire.mobile.alertsms
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import androidx.databinding.DataBindingUtil
 import com.ericampire.mobile.alertsms.databinding.ActivityMainBinding
-import com.nexmo.client.NexmoClient
-import com.nexmo.client.sms.MessageStatus
-import com.nexmo.client.sms.messages.TextMessage
-import org.koin.android.ext.android.inject
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,9 +15,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding
-    }
-
-    fun launchAlert(view: View) {
-
     }
 }
