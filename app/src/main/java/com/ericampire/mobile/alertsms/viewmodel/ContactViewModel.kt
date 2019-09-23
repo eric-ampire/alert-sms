0b0330453f16336777223d2f1988cb00767b0ef2
Eric Ampire <ericampire.top@gmail.com>
2019-09-23 17:25:10 +0200

    Add ContactViewModel.kt

---
+++ app/src/main/java/com/ericampire/mobile/alertsms/viewmodel/ContactViewModel.kt
@@ -0,0 +1,31 @@
+package com.ericampire.mobile.alertsms.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.ericampire.mobile.alertsms.model.Contact
+
+class ContactViewModel : ViewModel() {
+
+    private val _data = MutableLiveData<List<Contact>>()
+    val data: LiveData<List<Contact>>
+        get() = _data
+
+    fun delete(item: Contact) {
+        val oldData = data.value?.toMutableList() ?: mutableListOf()
+        oldData.remove(item)
+
+        _data.value = oldData
+    }
+
+    fun edit(item: Contact) {
+
+    }
+
+    fun add(item: Contact) {
+        val oldData = data.value?.toMutableList() ?: mutableListOf()
+        oldData.add(item)
+
+        _data.value = oldData
+    }
+}
