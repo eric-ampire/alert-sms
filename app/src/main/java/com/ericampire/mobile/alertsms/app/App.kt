e47efc48d621a3692ac17f84587d514ee89aecfa
Eric Ampire <ericampire.top@gmail.com>
2019-09-23 06:38:52 +0200

    Enabling Koin

---
+++ app/src/main/java/com/ericampire/mobile/alertsms/app/App.kt
@@ -0,0 +1,29 @@
+package com.ericampire.mobile.alertsms.app
+
+import android.app.Application
+import android.content.Context
+import androidx.multidex.MultiDex
+import com.ericampire.mobile.alertsms.app.di.appModule
+import com.ericampire.mobile.alertsms.app.di.nexmoModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        // Activation of koin
+        startKoin{
+            androidLogger()
+            androidContext(this@App)
+            modules(listOf(appModule, nexmoModule))
+        }
+    }
+
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
+        MultiDex.install(base)
+    }
+}
